2279b12e066cf63cfbd350f5ed8bba893912c796
fkaraagac11 <fehmi11.qa@gmail.com>
2020-10-10 21:53:20 -0400

    adding

--- Basic JS/CodeInterviewQuestions/FirstRepetationNumber.js
+++ Basic JS/CodeInterviewQuestions/FirstRepetationNumber.js
@@ -1,19 +1,38 @@
-//function type for solution 1
-//let arr = [3, 1, 2, 4, 3, 1, 2, 11];
-function mergeAndSortArrays(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        for (let j = 0; j < i; j++) {
-            if (arr[i] == arr[j]) {
-                console.log(`The first repeted number is ${arr[j]}`);
-                break;
-            } else {
-                console.log("no repetation in array");
+//Google Question
+//Given an array = [2,5,1,2,3,5,1,2,4]:
+//It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]:
+//It should return 1
+
+//Given an array = [2,3,4,5]:
+//It should return undefined
+
+function firstRecurringCharacter(input) {
+    for (let i = 0; i < input.length; i++) {
+        for (let j = i + 1; j < input.length; j++) {
+            if (input[i] === input[j]) {
+                return input[i];
             }
         }
     }
-    return arr3;
+    return undefined;
 }
 
-mergeAndSortArrays([124, 36, 17, 36, 99, 133, 244, 155, 99]);
+function firstRecurringCharacter2(input) {
+    let map = {};
+    for (let i = 0; i < input.length; i++) {
+        if (map[input[i]] !== undefined) {
+            return input[i];
+        } else {
+            map[input[i]] = i;
+        }
+    }
+    return undefined;
+}
 
-console.log(arr);
+firstRecurringCharacter2([1, 5, 5, 1, 3, 4, 6]);
+
+//Bonus... What if we had this:
+// [2,5,5,2,3,5,1,2,4]
+// return 5 because the pairs are before 2,2
