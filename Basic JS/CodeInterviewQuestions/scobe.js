d8d34e24df206f54e1f38f1dc0400913430d2aec
fkaraagac11 <fehmi11.qa@gmail.com>
2020-10-12 00:34:35 -0400

    adding

---
+++ Basic JS/CodeInterviewQuestions/scobe.js
@@ -0,0 +1,17 @@
+var b = "Balik";
+
+function bb() {
+    var b = "Kus";
+    return b;
+}
+
+function cc() {
+    b = "Tavsan";
+    return b;
+}
+
+console.log(b);
+console.log(bb());
+console.log(b);
+console.log(cc());
+console.log(b);
