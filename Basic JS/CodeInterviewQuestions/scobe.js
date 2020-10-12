6f6744a8fe17357fee1a78a69d34757bc4e9335b
fkaraagac11 <fehmi11.qa@gmail.com>
2020-10-12 19:55:58 -0400

    adding

--- Basic JS/CodeInterviewQuestions/scobe.js
+++ Basic JS/CodeInterviewQuestions/scobe.js
@@ -15,3 +15,24 @@ console.log(bb());
 console.log(b);
 console.log(cc());
 console.log(b);
+
+// OUTPUT:
+// Balik
+// Kus
+// Balik
+// Tavsan
+// Tavsan
+
+let arr = [1, 2, 4, 11];
+let newArr = [];
+arr.forEach((num) => {
+    newArr.push(num * 2);
+});
+console.log(newArr);
+
+const arr = [1, 2, 4, 11];
+const newArr = [];
+arr.forEach((num) => {
+    newArr.push(num * 2);
+});
+console.log(newArr);
